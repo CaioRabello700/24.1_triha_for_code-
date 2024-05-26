58dd39607e43cc8a949e2a2698011cba2ec582c5
CaioRabello700 <134748887+CaioRabello700@users.noreply.github.com>
2024-05-26 23:14:42 +0000

    criando arquivo trilha.py

---
+++ trilha.py
@@ -0,0 +1,15 @@
+#Inteiros e pontos flutuantes 
+
+# soma e subtração: + e -
+# multiplicação: *
+# divisão inteira: //
+# Resto de divisão: %
+
+varl = 11%10  # % modulo, retorna o resto da divisão
+varl2 = 2**3
+varl3 = 2**(1/2)
+print (varl3)
+
+
+
+ 
